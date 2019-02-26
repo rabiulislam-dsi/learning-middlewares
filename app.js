eaa7f0dc83b4445987ce7706391f2eedb851da15
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2019-02-26 12:58:50 +0600

    added error middleware in app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //require the packages
-const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -15,14 +14,12 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-//use middlewares
+// my middleware for logging
 app.use(function(req, res, next) {
-  console.log(`Request => ${req.method} ${req.url} ${res.status.statusCode}`);
-  req.on("end", () => {
-    console.log(res.status.statusCode);
-  });
+  console.log(`Request => ${req.method} ${req.url} `);
   next();
 });
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,20 +31,11 @@ app.use("/", indexRouter);
 app.use("/students", studentsRouter);
 app.use("/about", aboutRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
+// error handler middleware
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  console.log(err);
+  res.status(err.status);
+  res.render("error", { error: err });
 });
 
 module.exports = app;
--- routes/students.js
+++ routes/students.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("../db/dbconfig");
+const createError = require("http-errors");
 /* GET users listing. */
 router.get("/", async function(req, res, next) {
   try {
     let students = await knex.select().table("students");
     // console.log(students);
     res.render("students", { students: students });
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    next(createError(500, err));
   }
 });
 
 router.post("/addnew", async function(req, res, next) {
-  res.status(500).send({ success: false });
-  // let newStudent = {
-  //   student_name: req.body.name,
-  //   student_grade: req.body.grade,
-  //   student_age: req.body.age,
-  //   student_image_path: req.body.image
-  // };
-  // knex("students")
-  //   .insert(newStudent)
-  //   .then(function(affectedRows) {
-  //     return knex.select().table("students");
-  //   })
-  //   .then(function(students) {
-  //     res.render("students", { students: students });
-  //   })
-  //   .catch(function(err) {
-  //     console.log(err);
-  //   });
+  let newStudent = {
+    student_name: req.body.name,
+    student_grade: req.body.grade,
+    student_age: req.body.age,
+    student_image_path: req.body.image
+  };
+  knex("students")
+    .insert(newStudent)
+    .then(function(affectedRows) {
+      return knex.select().table("students");
+    })
+    .then(function(students) {
+      res.render("students", { students: students });
+    })
+    .catch(function(err) {
+      next(createError(500, err));
+    });
 });
 
 module.exports = router;
